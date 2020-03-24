44b605594ccf2a497041ae87fae84daa48a7cf8c
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-03-24 17:23:41 +0530

    about us and removed members

---
+++ about/effects.js
@@ -0,0 +1,34 @@
+var we = document.getElementById("we");
+
+window.onload = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    setTimeout(function() {
+        and.style.visibility = "visible";
+    }, 1000);
+}
+
+var mybutton = document.getElementById("tothetop");
+
+window.onscroll = function() {
+    scrollFunctiontop();
+    // imgfunction();
+};
+
+function scrollFunctiontop() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// imgscroll = document.getElementById("teamimg");
+
+// function imgfunction() {
+//     if (document.body.scrollTop > 100 && document.body.scrollTop < 500 || document.documentElement.scrollTop > 100 && document.documentElement.scrollTop < 500) {
+//         imgscroll.style.transform = "scale(1.2)";
+//     }
+// }
--- about/main.js
+++ about/main.js
@@ -38,22 +38,3 @@ cn.addEventListener('click', () => {
     cn.classList.toggle('navpostrans');
     bu.classList.toggle('block');
 });
-
-var mybutton = document.getElementById("tothetop");
-
-window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
