26c1be85669e3eb0d712a0b0881576c24aa97658
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-04-16 22:02:18 +0530

    effects in about us

--- about/effects.js
+++ about/effects.js
@@ -1,19 +1,10 @@
 var we = document.getElementById("we");
 
-window.onload = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    setTimeout(function() {
-        and.style.visibility = "visible";
-    }, 1000);
-}
-
 var mybutton = document.getElementById("tothetop");
 
 window.onscroll = function() {
     scrollFunctiontop();
+    marginfunction();
     // imgfunction();
 };
 
@@ -30,10 +21,45 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-// imgscroll = document.getElementById("teamimg");
+var row1 = document.getElementById('row1');
+var row2 = document.getElementById('row2');
+var row3 = document.getElementById('row3');
 
-// function imgfunction() {
-//     if (document.body.scrollTop > 100 && document.body.scrollTop < 500 || document.documentElement.scrollTop > 100 && document.documentElement.scrollTop < 500) {
-//         imgscroll.style.transform = "scale(1.2)";
-//     }
-// }
+var mq = window.matchMedia("(max-width: 766px)");
+if (mq.matches) {
+    function marginfunction() {
+        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
+            row1.style.position = "static";
+            row1.style.marginTop = "0vh";
+        } else {
+            row1.style.position = "static";
+            row1.style.marginTop = "0px";
+        }
+        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
+            row2.style.position = "static";
+            row2.style.marginTop = "0vh";
+        } else {
+            row2.style.position = "static";
+            row2.style.marginTop = "0px";
+        }
+        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
+            row3.style.position = "static";
+            row3.style.marginTop = "0vh";
+        } else {
+            row3.style.position = "static";
+            row3.style.marginTop = "0px";
+        }
+    }
+} else {
+    row1.style.position = "absolute";
+
+    function marginfunction() {
+        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
+            row1.style.position = "fixed";
+            row1.style.marginTop = "-109vh";
+        } else {
+            row1.style.position = "absolute";
+            row1.style.marginTop = "0vh";
+        }
+    }
+}
