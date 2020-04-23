cdd4e5eb57f6b6fecedb046ec878c467d88f6b64
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-04-24 00:23:15 +0530

    about us effects

--- about/effects.js
+++ about/effects.js
@@ -5,7 +5,6 @@ var mybutton = document.getElementById("tothetop");
 window.onscroll = function() {
     scrollFunctiontop();
     marginfunction();
-    // imgfunction();
 };
 
 function scrollFunctiontop() {
@@ -24,12 +23,7 @@ function topFunction() {
 var leftparent = document.getElementById('leftparent');
 var container = document.getElementById('container');
 const row1 = document.getElementById('row1');
-const row2 = document.getElementById('row2');
-const row3 = document.getElementById('row3');
-// const bigcol1 = document.getElementById('bigcol1');
-// const bigcol2 = document.getElementById('bigcol2');
-// const bigcol3 = document.getElementById('bigcol3');
-const lastdiv = document.getElementById('lastdiv');
+
 
 var mq = window.matchMedia("(max-width: 766px)");
 if (mq.matches) {
@@ -41,52 +35,38 @@ if (mq.matches) {
             row1.style.position = "static";
             row1.style.marginTop = "0px";
         }
-        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
-            row2.style.position = "static";
-            row2.style.marginTop = "0vh";
-        } else {
-            row2.style.position = "static";
-            row2.style.marginTop = "0px";
-        }
-        if (document.body.scrollTop > 787 || document.documentElement.scrollTop > 787) {
-            row3.style.position = "static";
-            row3.style.marginTop = "0vh";
-        } else {
-            row3.style.position = "static";
-            row3.style.marginTop = "0px";
-        }
     }
 } else {
 
     function marginfunction() {
-        if (document.body.scrollTop > 797 || document.body.scrollTop > 2375 || document.body.scrollTop > 3963 || document.documentElement.scrollTop > 797 || document.documentElement.scrollTop > 2375 || document.documentElement.scrollTop > 3963) {
+        if (document.body.scrollTop > window.innerHeight * 1.10326 || document.body.scrollTop > window.innerHeight * 3.2876 || document.body.scrollTop > window.innerHeight * 5.4858 || document.documentElement.scrollTop > window.innerHeight * 1.10326 || document.documentElement.scrollTop > window.innerHeight * 3.2876 || document.documentElement.scrollTop > window.innerHeight * 5.4858) {
             // leftparent.style.marginLeft = "-15px";
             leftparent.style.position = "fixed";
             leftparent.style.top = "0px";
             leftparent.style.marginTop = "0vh";
-            if (document.body.scrollTop > 1653 || document.documentElement.scrollTop > 1653) {
+            if (document.body.scrollTop > window.innerHeight * 2.2882 || document.documentElement.scrollTop > window.innerHeight * 2.2882) {
                 // leftparent.style.marginLeft = "0px";
                 leftparent.style.position = "absolute";
                 leftparent.style.marginTop = "230vh";
             }
-            if (document.body.scrollTop > 2375 || document.documentElement.scrollTop > 2375) {
+            if (document.body.scrollTop > window.innerHeight * 3.2876 || document.documentElement.scrollTop > window.innerHeight * 3.2876) {
                 // container.style.marginLeft = "0px";
                 leftparent.style.position = "fixed";
                 leftparent.style.bottom = "0";
                 leftparent.style.marginTop = "-100vh";
             }
-            if (document.body.scrollTop > 3197 || document.documentElement.scrollTop > 3197) {
+            if (document.body.scrollTop > window.innerHeight * 4.4255 || document.documentElement.scrollTop > window.innerHeight * 4.4255) {
                 // container.style.marginLeft = "15px";
                 leftparent.style.position = "absolute";
                 leftparent.style.marginTop = "347vh";
             }
-            if (document.body.scrollTop > 3963 || document.documentElement.scrollTop > 3963) {
+            if (document.body.scrollTop > window.innerHeight * 5.4858 || document.documentElement.scrollTop > window.innerHeight * 5.4858) {
                 // container.style.marginLeft = "0px";
                 leftparent.style.position = "fixed";
                 leftparent.style.bottom = "0";
                 leftparent.style.marginTop = "-200vh";
             }
-            if (document.body.scrollTop > 4841 || document.documentElement.scrollTop > 4841) {
+            if (document.body.scrollTop > window.innerHeight * 6.7012 || document.documentElement.scrollTop > window.innerHeight * 6.7012) {
                 // container.style.marginLeft = "15px";
                 leftparent.style.position = "absolute";
                 leftparent.style.marginTop = "484.7vh";
@@ -97,8 +77,41 @@ if (mq.matches) {
             leftparent.style.bottom = "initial";
             leftparent.style.marginTop = "0vh";
         }
-        // else {
+        // if (document.body.scrollTop > window.innerHeight*1.10326 || document.body.scrollTop > 2375 || document.body.scrollTop > 3963 || document.documentElement.scrollTop > window.innerHeight*1.10326 || document.documentElement.scrollTop > 2375 || document.documentElement.scrollTop > 3963) {
+        //     // leftparent.style.marginLeft = "-15px";
+        //     leftparent.style.position = "fixed";
+        //     leftparent.style.top = "0px";
+        //     leftparent.style.marginTop = "0vh";
+        //     if (document.body.scrollTop > 1653 || document.documentElement.scrollTop > 1653) {
+        //         // leftparent.style.marginLeft = "0px";
+        //         leftparent.style.position = "absolute";
+        //         leftparent.style.marginTop = "230vh";
+        //     }
+        //     if (document.body.scrollTop > 2375 || document.documentElement.scrollTop > 2375) {
+        //         // container.style.marginLeft = "0px";
+        //         leftparent.style.position = "fixed";
+        //         leftparent.style.bottom = "0";
+        //         leftparent.style.marginTop = "-100vh";
+        //     }
+        //     if (document.body.scrollTop > 3197 || document.documentElement.scrollTop > 3197) {
+        //         // container.style.marginLeft = "15px";
+        //         leftparent.style.position = "absolute";
+        //         leftparent.style.marginTop = "347vh";
+        //     }
+        //     if (document.body.scrollTop > 3963 || document.documentElement.scrollTop > 3963) {
+        //         // container.style.marginLeft = "0px";
+        //         leftparent.style.position = "fixed";
+        //         leftparent.style.bottom = "0";
+        //         leftparent.style.marginTop = "-200vh";
+        //     }
+        //     if (document.body.scrollTop > 4841 || document.documentElement.scrollTop > 4841) {
+        //         // container.style.marginLeft = "15px";
+        //         leftparent.style.position = "absolute";
+        //         leftparent.style.marginTop = "484.7vh";
+        //     }
+        // } else {
         //     leftparent.style.position = "absolute";
+        //     leftparent.style.top = "initial";
         //     leftparent.style.bottom = "initial";
         //     leftparent.style.marginTop = "0vh";
         // }
